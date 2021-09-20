3f77c674b775276f3242b1c3350633e2a2f9bcfd
Carlos Enrique Gutiérrez Juárez <75094293+carlos28d@users.noreply.github.com>
2021-09-20 17:31:18 -0500

    first commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+using TodoApi.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.API.Data{
+    public class DataContext : DbContext {
+        public DataContext(DbContextOptions options) : base(options){
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
